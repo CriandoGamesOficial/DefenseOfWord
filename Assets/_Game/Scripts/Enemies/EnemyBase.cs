d461e6c469b46ce4ec631d0707dc29c0b31d5284
Carlos Castro <carloscastrogames@gmail.com>
2018-09-03 00:42:49 -0300

    Criando Enimes Base

--- Assets/_Game/Scripts/Enemies/EnemyBase.cs
+++ Assets/_Game/Scripts/Enemies/EnemyBase.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 
-
-namespace Assets.Scripts.Enimies
+namespace Assets.Scripts.Enemies
 {
 
     abstract class EnemyBase: MonoBehaviour
     {
         [SerializeField]
         protected float Speed;
+
         [SerializeField]
         protected float SpeedShot;
+
+        [SerializeField]
+        protected float Speeds;
+
+        [SerializeField]
+        protected GameObject ExplosionAimation;
+
+        [SerializeField]
+        protected GameObject ExplosionPosition;
+
+        protected virtual void SetDestroy(float time)
+        {
+          var obj =  Instantiate(ExplosionAimation);
+          obj.transform.SetParent(ExplosionPosition.transform);
+          Destroy(gameObject,time);
+        }
+       
+       public abstract void SetShot();
+
 
     }
 
